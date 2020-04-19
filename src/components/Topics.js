ac4781008bae4c87ecbd05578caba9aef2555ac4
GermanVor <vorotnikov.german@yandex.ru>
2020-04-20 01:08:03 +0300

    +Prettier

--- src/components/Topics.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//странный способ вложенности,скорее всего практиковался 
-export default ({CheckWrapper, subredditArray}) => (
-    <ul className='Topics'>
-        { subredditArray.map( (a,ind) => <li key={'Topic_key_+' + ind } >{ CheckWrapper(a)}</li> )}
-    </ul>
-)
- 
